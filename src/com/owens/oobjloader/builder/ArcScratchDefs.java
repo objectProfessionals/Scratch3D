94a14bbc4e238468076f1c9705d9e2ee4d1e7a58
objectProfessionals <objectProfessionals@googlemail.com>
2017-02-11 21:45:18 +0000

    Forgot

---
+++ src/com/owens/oobjloader/builder/ArcScratchDefs.java
@@ -0,0 +1,12 @@
+package com.owens.oobjloader.builder;
+
+import java.util.ArrayList;
+
+public class ArcScratchDefs {
+
+	public double cx;
+	public double cy;
+	public double r;
+	public double startPosAng;
+	public ArrayList<Boolean> arcs = new ArrayList<Boolean>();
+}
